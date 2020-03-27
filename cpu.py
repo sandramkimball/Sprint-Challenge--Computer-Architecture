25f1b8a89b0030cf7e172c7ff117cc67696aec3b
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-27 09:28:22 -0700

    began change to branchtables

--- cpu.py
+++ cpu.py
@@ -2,27 +2,7 @@
 
 import sys
 
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
-MUL = 0b10100010 
-PUSH = 0b01000101
-POP = 0b01000110
-CALL = 0b01010000
-RET  = 0b00010001
-LD   = 0b10000011
-ST   = 0b10000100
-PRA  = 0b01001000
-ADD  = 0b10100000
-CMP = 0b10100111
-JMP = 0b01010100
-JEQ = 0b01010101
-JNE = 0b01010110
 
-
-# - [ ] Add the `CMP` instruction and `equal` flag to your LS-8.
-# - [ ] Add the `JMP` instruction.
-# - [ ] Add the `JEQ` and `JNE` instructions.
 
 class CPU:
     def __init__(self):
@@ -30,6 +10,36 @@ class CPU:
         self.reg = [0] * 8 
         self.pc = 0 # Program Counter
         self.SP = 0xF4 # Stack Pointer, register[SP]
+        self.FL = 0 #? flag
+        
+        self.branchtable = {
+            #funcs indexed by opcode val
+            HLT = 0b00000001
+            LDI = 0b10000010
+            PRN = 0b01000111
+            MUL = 0b10100010 
+            PUSH = 0b01000101
+            POP = 0b01000110
+            CALL = 0b01010000
+            RET  = 0b00010001
+            ADD  = 0b10100000
+            CMP = 0b10100111
+            JMP = 0b01010100
+            JEQ = 0b01010101
+            JNE = 0b01010110
+        }
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[MUL] = self.handle_MUL
+        self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[POP] = self.handle_POP
+        self.branchtable[CALL] = self.handle_CALL
+        self.branchtable[RET] = self.handle_RET
+        self.branchtable[ADD] = self.handle_ADD
+        self.branchtable[CMP] = self.handle_CMP
+        self.branchtable[JMP] = self.handle_JMP
+        self.branchtable[JEQ] = self.handle_JEQ
+        self.branchtable[JNE] = self.handle_JNE
 
     def load(self): 
         """Load a program into memory."""
@@ -108,7 +118,7 @@ class CPU:
         # self.pc += 3
 
     def run(self):
-        # if/elif = O(n) || O(1)
+        # if/elif = O(n) || branchtable = O(1)
         # read address in pc, store result in IR (Instruction Register)
         running = True
         while running is True:
@@ -116,61 +126,13 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1) #address = register[pc + 1]
             operand_b = self.ram_read(self.pc + 2)
 
-            # HLT
-            if IR == 0b00000001: 
-                running = False
-                # self.pc += 1
+            # fetch instruction val from RAM
+            # use val to look up, call func in branch table
+            IR = OP1
+            self.branchtable[IR](operand_a)
 
-            # LDI
-            elif IR == 0b10000010: 
-                # load immediate, store val in reg
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-
-            # PRN
-            elif IR == 0b01000111:
-                # prints num value in rgstr
-                print(self.reg[operand_a])
-                self.pc += 2
- 
-            # MUL
-            elif IR == 0b10100010:
-                # self.alu(MUL, operand_a, operand_b)
-                self.reg[operand_a] *= self.reg[operand_b]
-                self.pc += 3
-
-            # PUSH
-            elif IR == 0b01000101:
-                # Copy val from address to given reg
-                # decrement SP
-                self.SP -= 1
-                self.ram[self.SP] = self.reg[operand_a]
-                self.pc += 2
-
-            # POP
-            elif IR == 0b01000110:
-                # copy val from address
-                # increment SP
-                self.reg[operand_a] = self.ram[self.SP]
-                self.SP += 1
-                self.pc += 2
-
-            # CALL
-            elif IR == 0b01010000:
-                # decrement SP
-                self.SP -= 1
-                self.ram[self.SP] = self.pc + 2
-                # store the instruction
-                self.pc = self.reg[operand_a]
-
-            # RET
-            elif IR == 0b00010001:
-                # pop val from stack and return 
-                self.pc = self.ram[self.SP]
-                self.SP += 1
-
-            # ADD
-            elif IR == 0b10100000:
+            IR = OP2
+            self.branchtable[IR](operand_b)
                 # pop val from stack and store in PC
                 self.reg[operand_a] += self.reg[operand_b]
                 self.pc += 3
@@ -181,90 +143,99 @@ class CPU:
                 sys.exit(1)
 
 
-def HLT(self):
-    running = False
-    # self.pc += 1
+    def handle_HLT(self):
+        running = False
+        # self.pc += 1
 
-def PRN(self):
-    # prints num value in rgstr
-    print(self.reg[operand_a])
-    self.pc += 2
+    def handle_PRN(self):
+        # prints num value in rgstr
+        print(self.reg[operand_a])
+        self.pc += 2
 
-def LDI(self):
-    # load immediate, store val in reg
-    self.reg[operand_a] = operand_b
-    self.pc += 3
+    def handle_LDI(self):
+        # load immediate, store val in reg
+        self.reg[operand_a] = operand_b
+        self.pc += 3
 
-def MUL(self):
-    # self.alu(MUL, operand_a, operand_b)
-    self.reg[operand_a] *= self.reg[operand_b]
-    self.pc += 3
+    def handle_MUL(self):
+        # self.alu(MUL, operand_a, operand_b)
+        self.reg[operand_a] *= self.reg[operand_b]
+        self.pc += 3
 
-def PUSH(self):
-    # Copy val from address to given reg
-    # decrement SP
-    self.SP -= 1
-    self.ram[self.SP] = self.reg[operand_a]
-    self.pc += 2
+    def handle_PUSH(self):
+        # Copy val from address to given reg
+        # decrement SP
+        self.SP -= 1
+        self.ram[self.SP] = self.reg[operand_a]
+        self.pc += 2
 
-def POP(self):
-    # copy val from address
-    # increment SP
-    self.reg[operand_a] = self.ram[self.SP]
-    self.SP += 1
-    self.pc += 2
+    def handle_POP(self):
+        # copy val from address
+        # increment SP
+        self.reg[operand_a] = self.ram[self.SP]
+        self.SP += 1
+        self.pc += 2
 
-def CALL(self):
-    # decrement SP
-    self.SP -= 1
-    self.ram[self.SP] = self.pc + 2
-    # store the instruction
-    self.pc = self.reg[operand_a]
+    def handle_CALL(self):
+        # decrement SP
+        self.SP -= 1
+        self.ram[self.SP] = self.pc + 2
+        # store the instruction
+        self.pc = self.reg[operand_a]
 
-def RET(self):
-    # pop val from stack and return 
-    self.pc = self.ram[self.SP]
-    self.SP += 1
+    def handle_RET(self):
+        # pop val from stack and return 
+        self.pc = self.ram[self.SP]
+        self.SP += 1
 
-def ADD(self):
+    def handle_ADD(self):
+        # pop val from stack and store in PC
+        self.reg[operand_a] += self.reg[operand_b]
+        self.pc += 3
 
-def CMP(self):
-    #handled by alu
-    if register_a = register_b:
-        # set flag to equal
-        'E' = 1
-        'L' = 0
-        'G' = 0
-    if register_a < register_b:
-        # set flag to L
-        'E' = 0
-        'L' = 1
-        'G' = 0
-    if register_a > register_b:
-        # set flag to G
-        'E' = 0
-        'L' = 0
-        'G' = 1
+    def handle_CMP(self, op, reg_a, reg_b):
+        #handled by alu
+        reg_a = self.reg[operand_a]
+        reg_b = self.reg[operand_b]
 
-def JMP(self):
-    # jump to address stored in given reg
-    # set PC to this address
-    self.pc = register[self.SP]
+        if reg_a == reg_b:
+            # set flag to equal
+            self.FL = 'E'
+            self.E = 1
+            self.L = 0
+            self.G = 0
+        if reg_a < reg_b:
+            # set flag to L
+            self.FL = 'L'
+            self.E = 0
+            self.L = 1
+            self.G = 0
+        if reg_a > reg_b:
+            # set flag to G
+            self.FL = 'G'
+            self.E = 0
+            self.L = 0
+            self.G = 1
 
-def JEQ(self):
-    #if flag = Equal, jump to address stored in given reg
-    if 'E' = 1:
-        self.JMP()
-    else:
-        continue
+    def handle_JMP(self):
+        # jump to address stored in given reg
+        # set PC to this address
+        self.pc = register[self.SP]
 
-def JNE(self):
-    # if flag is not Equal, jump to address in given reg
-    if 'E' = 0:
-        self.JMP()
-    else:
-        continue
+    def handle_JEQ(self):
+        #if flag = Equal, jump to address stored in given reg
+        if self.E == 1:
+            self.JMP()
+        else:
+            continue
 
-    # pop val from stack and store in PC
-    self.reg[operand_a] += self.reg[operand_b]
-    self.pc += 3
+    def handle_JNE(self):
+        # if flag is not Equal, jump to address in given reg
+        if self.E = 0:
+            self.JMP()
+        else:
+            continue
+
+        # pop val from stack and store in PC
+        self.reg[operand_a] += self.reg[operand_b]
+        self.pc += 3
